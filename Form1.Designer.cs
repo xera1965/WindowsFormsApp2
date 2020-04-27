db3968e0868831fe13ada114d5e0a008310d807a
Xera Giputxi <xera1965@outlook.es>
2020-04-27 15:38:52 +0200

    provisional. no me deja otra forma

---
+++ Form1.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class PowerUnderKnow_PUNK_Tv
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PowerUnderKnow_PUNK_Tv));
+            this.SuspendLayout();
+            // 
+            // PowerUnderKnow_PUNK_Tv
+            // 
+            this.AccessibleDescription = "P.UN.K Tv";
+            this.AccessibleName = "P.UN.K Tv";
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(617, 450);
+            this.HelpButton = true;
+            this.Name = "PowerUnderKnow_PUNK_Tv";
+            this.Text = "Power Under Know Tv";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.PowerUnderKnow_PUNK_Tv_Paint);
+            this.Resize += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class PowerUnderKnow_PUNK_Tv : Form
+    {
+        public PowerUnderKnow_PUNK_Tv()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PowerUnderKnow_PUNK_Tv_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
